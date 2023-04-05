9cf1d9d9b7d1b56fd7c164f05299c99f75a876c7
agan-k <koranagan@gmail.com>
2023-04-05 10:15:30 -0400

    add footer content

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,20 @@
+import {Link} from 'react-router-dom';
+import {Box, Text} from '../shared';
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <>
-      <hr></hr>
-      <p>Footer...</p>
+      <Box display={'flex'} justifyContent={'center'}>
+        ©<span>{currentYear}</span>&nbsp;Dominique Carre
+      </Box>
+      <Box display={'flex'} justifyContent={'center'}>
+        <Text color={'lightGray'}>site by &nbsp;</Text>
+        <Link to={'https://formversuscontent.com'} target='_blank'>
+          <Text color={'primary'}>FormVsContent</Text>
+        </Link>
+      </Box>
     </>
   );
 }
+
