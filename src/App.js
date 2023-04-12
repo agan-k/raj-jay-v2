e41de98333999b60d81af8dd6ced2175c6eed0d5
agan-k <koranagan@gmail.com>
2023-04-11 22:00:44 -0400

    add global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from 'react-router-dom';
 import {Footer, Header} from './layout';
+import {GlobalStyle} from './theme';
 
 export default function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Outlet />
       <Footer />
     </>
-  )
+  );
 }
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle(
+  {
+    body: {
+      maxWidth: '800px',
+      margin: '0 auto',
+    },
+  }
+);
