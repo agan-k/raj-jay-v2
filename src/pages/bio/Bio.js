c53483cd1030e9161173d166cc5fa1e1a51d3dff
agan-k <koranagan@gmail.com>
2023-04-11 21:49:14 -0400

    render bio data

--- src/pages/bio/Bio.js
+++ src/pages/bio/Bio.js
@@ -1,3 +1,16 @@
+import {useSinglePrismicDocument, PrismicRichText} from '@prismicio/react';
+
 export default function Bio() {
-  return <h1>Bio</h1>;
+  //TODO: Validate fields
+  const bioData = useSinglePrismicDocument('bio');
+  const imageUrl = bioData[0]?.data.bio_image.url;
+  const bio = bioData[0]?.data.bio;
+  return (
+    <>
+      <h1>Bio</h1>
+      <img src={imageUrl} width='100px' />
+      <PrismicRichText field={bio} />
+    </>
+
+  );
 }
