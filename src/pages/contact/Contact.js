64efd6ef8932141115343350e0e154a5c64910b9
agan-k <koranagan@gmail.com>
2023-04-11 21:22:00 -0400

    clean up

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -17,7 +17,7 @@ export default function Contact() {
       <Box m={4}>{publicists}</Box>
       <Box m={4}>
         <form name='contact' method='POST' data-netlify='true'>
-          <input type="hidden" name="form-name" value="contact" />
+          <input type='hidden' name='form-name' value='contact' />
           <input type='text' name='name' placeholder='name' />      
           <input type='email' name='email' placeholder='e-mail' />      
           <textarea name='message'></textarea>
