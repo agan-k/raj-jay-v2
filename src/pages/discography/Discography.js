61f20f4cf202ec8108ea77a03012ed0de6f4c900
agan-k <koranagan@gmail.com>
2023-04-13 21:18:51 -0400

    render albums

--- src/pages/discography/Discography.js
+++ src/pages/discography/Discography.js
@@ -1,4 +1,5 @@
 import {useAllPrismicDocumentsByType, PrismicRichText} from '@prismicio/react';
+import {Box, SmallHeading} from '../../shared';
 
 export default function Discography() {
   const discographyData = useAllPrismicDocumentsByType('album', {
@@ -9,8 +10,37 @@ export default function Discography() {
       },
     ],
   });
+
+  const albums = discographyData[0]?.map((album) => {
+    const lineup = album.data.lineup.map((l, index) =>
+      <li key={l.name + index}>
+        <span>{l.name}&nbsp;-&nbsp;</span>
+        <span>{l.instrument}</span>
+      </li>
+    );
+    const hasLineup = Boolean(album.data.lineup.length !== 0);
+    const label = album.data.label || null;
+    const notes = album.data.notes || null;
+    const hasLabel = Boolean(album.data.label !== null);
+    const hasNotes = Boolean(album.data.notes !== null);
+
+    return (
+      <Box key={album.id} mb={6} borderBottom={'1px solid grey'}>
+        <img src={album.data.artwork.url}  width='200px'/>
+        <SmallHeading mb={0}>{album.data.artist}</SmallHeading>
+        <span>{album.data.title}</span>
+        {hasLabel ?  <span>&nbsp;-&nbsp;{label}</span> : ''}
+       
+        {hasLineup ? <ul>{lineup}</ul> : ''}
+        {hasNotes ? <PrismicRichText field={notes} /> : ''}
+      </Box>
+    );
+  });
   
   return (
-    <h1>Discography</h1>
+    <>
+      <h1>Discography</h1>
+      {albums}
+    </>
   );
 }
---
+++ src/shared/SmallHeading.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import {
+  space, 
+  color, 
+  typography,
+  layout,
+} from 'styled-system';
+
+const SmallHeading = styled('h2')(
+  space,
+  color,
+  typography,
+  layout
+);
+export default SmallHeading;
