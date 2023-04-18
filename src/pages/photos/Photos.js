47cc0b43bcbbbdea052c7174f94fa3ea009405f9
agan-k <koranagan@gmail.com>
2023-04-17 22:12:51 -0400

    render photos data in Photos page

--- src/pages/photos/Photos.js
+++ src/pages/photos/Photos.js
@@ -1,3 +1,12 @@
+import {useAllPrismicDocumentsByType} from '@prismicio/react';
+import{Box} from '../../shared';
+
 export default function Photos() {
-  return <h1>Photos</h1>;
+  const photosData = useAllPrismicDocumentsByType('photo');
+  const photos = photosData[0]?.map((photo) =>
+    <img key={photo.id} src={photo.data.image.url} width="300px"/>
+  );
+  return (
+    <Box mt={5}>{photos}</Box>
+  );
 }
