6ba4932bcd1275843128c4ecaa17b7db9aa63d70
agan-k <koranagan@gmail.com>
2023-04-24 11:13:13 -0400

    add CTA to Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,37 @@
-// import {useAllPrismicDocumentsByType, PrismicRichText} from '@prismicio/react';
-import {HomeWrapper} from './styled';
-
+import {useSinglePrismicDocument} from '@prismicio/react';
+import {CTAWrapper, HomeWrapper} from './styled';
+import {Box, Text, Flex, SmallHeading} from '../../shared';
+import {theme} from '../../theme';
 
 export default function Home() {
-  
+  const [ctaData] = useSinglePrismicDocument('home_cta');
+  const imageUrl = ctaData?.data.image.url;
+  const description = ctaData?.data.description;
+  const CtaButton = () => {
+    return(
+      <Text 
+        fontSize={theme.fontSizes[4]}
+        backgroundColor={theme.colors.primary}
+        padding={4}
+        textAlign={'center'}
+      >
+        {ctaData?.data.button}
+      </Text>
+    );
+  };
   return (
     <HomeWrapper>
+      <CTAWrapper>
+        <Flex>
+          <Box flex={'1 0 40%'}>
+            <img src={imageUrl} />
+          </Box>
+          <Box flex={'1 0 40%'}>
+            <SmallHeading>{description}</SmallHeading>
+            <CtaButton/>
+          </Box>
+        </Flex>
+      </CTAWrapper>
     </HomeWrapper>
   ); 
 }
--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import {Box} from '../../shared';
 import {mediaQuery} from '../../utils';
+import {theme} from '../../theme';
 
 export const HomeWrapper = styled(Box)(
   {
@@ -8,6 +9,19 @@ export const HomeWrapper = styled(Box)(
   },
   mediaQuery(
     {
+    }
+  )
+);
+export const CTAWrapper = styled(Box)(
+  {
+    img: {
+      width: theme.space[8],
+    },
+  },
+  mediaQuery(
+    {
+      width: ['', '', '50%'],
+      padding: ['', '', theme.space[6]],
     }
   )
 );
