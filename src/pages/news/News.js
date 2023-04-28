921810c544bd000780b8f626c977d3766f00ea60
agan-k <koranagan@gmail.com>
2023-04-27 22:29:48 -0400

    create top feed row with three items

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -15,24 +15,45 @@ export default function News() {
       ],
     });
 
-  const filteredFeedData = feedData[0]?.filter((item, index) => index !== 0);
-  const mostRecentItem = feedData[0]?.[0];
-
-  const FirstFeedItem = () => {
-    return (
-      <FeedItemWrapper className='feed-item'>
-        <img src={mostRecentItem?.data.image.url} width={'100%'} />
-        <Box p={4} pt={0}>
-          <h3>{mostRecentItem?.data.date}</h3>
-          <h2>{mostRecentItem?.data.title}</h2>
-          <PrismicRichText field={mostRecentItem?.data.description}/>
+  const filteredFeedData = feedData[0]?.filter((item, index) => index > 2);
+  const firstThreeItems = feedData[0]?.filter((item, index) => index < 3);
+  const topItems = firstThreeItems?.map((item, index) => {
+    if(index > 0) {
+      return(
+        <FeedItemWrapper key={item.id}>
+          <img src={item.data.image.url} width={'100%'} />
+          <Box p={4} pt={0}>
+            <h3>{item.data.date}</h3>
+            <h2>{item.data.title}</h2>
+            <PrismicRichText field={item.data.description}/>
+          </Box>
+        </FeedItemWrapper>
+      );
+    }
+    return(
+      <FeedItemWrapper key={item.id} display={'flex'} >
+        <Box maxHeight={'300px'} overflow={'hidden'}>
+          <img src={item.data.image.url} width={'100%'} />
         </Box>
+        <Box p={4} pt={0}>
+          <h3>{item.data.date}</h3>
+          <h2>{item.data.title}</h2>
+          <PrismicRichText field={item.data.description}/>
+        </Box>
+      </FeedItemWrapper>
+    );
+  }
+  );
+  const TopFeedRow = () => {
+    return (
+      <FeedItemWrapper display={'flex'} >
+        {topItems}
       </FeedItemWrapper>
     );
   };
 
   const feed = filteredFeedData?.map((item) =>
-    <FeedItemWrapper key={item.id} className='feed-item'>
+    <FeedItemWrapper key={item.id}>
       <img src={item.data.image.url} width={'100%'} />
       <Box p={4} pt={0}>
         <h3>{item.data.date}</h3>
@@ -45,7 +66,7 @@ export default function News() {
   return (
     <FeedWrapper>
       <h1>News:</h1>
-      <FirstFeedItem/>
+      <TopFeedRow />
       <MasonryWrapper>
         <Masonry
           breakpointCols={masonryBreakpoints(6)}
